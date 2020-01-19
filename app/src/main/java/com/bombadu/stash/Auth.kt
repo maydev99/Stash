575074aed0643f5e506c6d9861dd81982d446b83
michael <maydev99@gmail.com>
2020-01-19 18:10:13 -0500

    -Added Google Sign On -Added Email Verification - not quite working

--- app/src/main/java/com/bombadu/stash/Auth.kt
+++ app/src/main/java/com/bombadu/stash/Auth.kt
@@ -4,17 +4,25 @@ import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_auth.*
 
 class Auth : AppCompatActivity() {
 
     private var isCreatingNewAccount = false
-    private var auth: FirebaseAuth? = null
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +31,13 @@ class Auth : AppCompatActivity() {
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         auth = FirebaseAuth.getInstance()
 
+        //For Using Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        //Email Password Sign In Button
         signInButton.setOnClickListener {
             val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
@@ -38,21 +53,28 @@ class Auth : AppCompatActivity() {
 
             }
 
+        }
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        //GoogleSign In Button
+        gSignInButton.setOnClickListener {
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
 
         }
 
 
-
+        //Switches email Sign In to email Create Account
         createAccountTextView.setOnClickListener {
             if (!isCreatingNewAccount) {
-                titleTextView.text = getString(R.string.new_account)
+                //titleTextView.text = getString(R.string.new_account)
                 signInButton.text = getString(R.string.create_account)
-                createAccountTextView.text = getString(R.string.sign_in)
+                createAccountTextView.text = getString(R.string.tap_here_to_sign_in)
                 isCreatingNewAccount = true
             } else {
-                titleTextView.text = getString(R.string.stash)
+                //titleTextView.text = getString(R.string.stash)
                 signInButton.text = getString(R.string.sign_in)
-                createAccountTextView.text = getString(R.string.create_account)
+                createAccountTextView.text = getString(R.string.tap_here_to_create_an_account)
                 isCreatingNewAccount = false
             }
 
@@ -61,7 +83,7 @@ class Auth : AppCompatActivity() {
 
 
     }
-
+    //Checks email edit text and password edit text for empty fields
     private fun validateForm(email: String, password: String): Boolean {
 
         var valid = true
@@ -84,10 +106,14 @@ class Auth : AppCompatActivity() {
 
     }
 
+
+
+    //Handles the process of email sign in
     private fun signIn(email: String, password: String) {
         auth!!.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
+                    sendEmailVerification()
                     startActivity(Intent(this, StashList::class.java))
                     makeAToast("Sign-In Successful")
                     finish()
@@ -98,11 +124,35 @@ class Auth : AppCompatActivity() {
 
     }
 
+    private fun sendEmailVerification() {
+        val user = auth.currentUser
+        user?.sendEmailVerification()
+            ?.addOnCompleteListener(this) { task ->
+                // [START_EXCLUDE]
+                // Re-enable button
+                //verifyEmailButton.isEnabled = true
+
+                if (task.isSuccessful) {
+                    Toast.makeText(baseContext,
+                        "Verification email sent to ${user.email} ",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.e(TAG, "sendEmailVerification", task.exception)
+                    Toast.makeText(baseContext,
+                        "Failed to send verification email.",
+                        Toast.LENGTH_SHORT).show()
+                }
+                // [END_EXCLUDE]
+            }
+    }
+
+    //Handles the process of email create account
     private fun createNewAccount(email: String, password: String) {
         auth!!.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    startActivity(Intent(this, StashList::class.java))
+                    //startActivity(Intent(this, StashList::class.java))
+                    sendEmailVerification()
                     makeAToast("Account Created")
                     finish()
                 } else {
@@ -127,5 +177,52 @@ class Auth : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    companion object{
+        private const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9001
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                // ...
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + account.id!!)
+
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    val email = user?.email
+                    makeAToast("Sign In: Successful: $email")
+                    startActivity(Intent(this, StashList::class.java))
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    makeAToast("Sign In Failed")
+
+                }
+
+            }
     }
 }
--- app/src/main/java/com/bombadu/stash/PasswordReset.kt
+++ app/src/main/java/com/bombadu/stash/PasswordReset.kt
@@ -6,13 +6,12 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_auth.*
 import kotlinx.android.synthetic.main.activity_password_reset.*
+
 
 class PasswordReset : AppCompatActivity() {
 
-    //private var auth: FirebaseAuth? = null
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password_reset)
@@ -22,17 +21,23 @@ class PasswordReset : AppCompatActivity() {
         passwordResetButton.setOnClickListener {
 
 
-            var auth = FirebaseAuth.getInstance()
+            val auth = FirebaseAuth.getInstance()
             val emailText = resetEditText.text.toString()
             auth.sendPasswordResetEmail(emailText)
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
-                        Log.d("PASS RESET", "Email sent.")
-                        emailEditText.text = null
-                        Toast.makeText(this, "Email Sent", Toast.LENGTH_SHORT).show()
+                        Log.d("PASS RESET", "email sent.")
+                        finish()
+                        Toast.makeText(this, "email Sent", Toast.LENGTH_SHORT).show()
                     }
+                }
+                .addOnFailureListener{
+                    Toast.makeText(this, "email reset failed, check email address", Toast.LENGTH_SHORT).show()
+
+
                 }
 
         }
     }
+
 }
